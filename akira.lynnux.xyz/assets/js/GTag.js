21752810916f7e4ed8e8f7961190da8c6a76bd64
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-10-09 17:45:36 +0200

    de

---
+++ akira.lynnux.xyz/assets/js/GTag.js
@@ -0,0 +1,10 @@
+const script = document.createElement('script');
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-JNQ2FM3R1L';
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-JNQ2FM3R1L');

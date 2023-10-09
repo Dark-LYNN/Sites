cbdba229ece940b88ffd613d81eab7313f403a3a
Dark-LYNN <86672385+DarkLYNN@users.noreply.github.com>
2023-10-09 17:31:07 +0200

    de

---
+++ BB-BotList/assets/main.js
@@ -0,0 +1,8 @@
+function Toggle-Nav() {
+    var x = document.getElementById("Navbar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
